e5a322965552c3adb6a4e1c50a423c3feca984b1
Tom Looman <tom.looman@epicgames.com>
2017-11-09 15:51:21 +0100

    section 8 lecture 5

--- Source/CoopGame/Private/SGameMode.cpp
+++ Source/CoopGame/Private/SGameMode.cpp
@@ -1,7 +1,58 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SGameMode.h"
+#include "TimerManager.h"
 
 
 
+ASGameMode::ASGameMode()
+{
+	TimeBetweenWaves = 2.0f;
+}
 
+
+void ASGameMode::StartWave()
+{
+	WaveCount++;
+
+	NrOfBotsToSpawn = 2 * WaveCount;
+
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimerElapsed, 1.0f, true, 0.0f);
+}
+
+
+void ASGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	PrepareForNextWave();
+}
+
+
+void ASGameMode::PrepareForNextWave()
+{
+
+	FTimerHandle TimerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+
+void ASGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	PrepareForNextWave();
+}
+
+
+void ASGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	NrOfBotsToSpawn--;
+
+	if (NrOfBotsToSpawn <= 0)
+	{
+		EndWave();
+	}
+}
--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -14,7 +14,38 @@ class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected:
+
+	FTimerHandle TimerHandle_BotSpawner;
+
+	// Bots to spawn in current wave
+	int32 NrOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
 	
-	
-	
+protected:
+
+	// Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	// Start Spawning Bots
+	void StartWave();
+
+	// Stop Spawning Bots
+	void EndWave();
+
+	// Set timer for next startwave
+	void PrepareForNextWave();
+
+public:
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
 };
