d51726aba2c3b84a78fd13ba9611d87f35a6f794
Tom Looman <tom@lodzero.com>
2018-11-09 00:59:48 +0100

    Update to 4.20 (AI code fix, material fix)

--- Source/CoopGame/CoopGame.Build.cs
+++ Source/CoopGame/CoopGame.Build.cs
@@ -8,7 +8,7 @@ public class CoopGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
